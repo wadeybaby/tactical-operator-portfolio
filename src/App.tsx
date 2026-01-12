42245d2a045280ad8e73129449dd59704041dc0c
wadeybaby <wjoshua.britz@gmail.com>
2026-01-12 15:02:22 +0200

    Rename project to wadebritz_portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/tactical-operator-portfolio" element={<Index />} />
+            <Route path="/wadebritz_portfolio" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/tactical-operator-portfolio/",
+  base: "/wadebritz_portfolio/",
   server: {
     host: "::",
     port: 8080,
