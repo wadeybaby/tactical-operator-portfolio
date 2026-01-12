5c6f76649e77d74fb50d9bc678887b61025add0b
wadeybaby <wjoshua.britz@gmail.com>
2026-01-12 13:32:26 +0200

    Updated base 12 Jan

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/tactical-operator-portfolio/",
+  base: "https://wadeybaby.github.io/",
   server: {
     host: "::",
     port: 8080,
