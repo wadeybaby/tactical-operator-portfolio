3e49fd3ed3ac9f3c1fbca729f5468514347b8d53
wadeybaby <wjoshua.britz@gmail.com>
2026-01-12 14:40:21 +0200

    Add basename to router

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/tactical-operator-portfolio" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
